0fdf790deeb0117b0b98cbe99f7f3c10df9bcfa7
trissssssssssssssssss <tristandehalleux@gmail.com>
2025-11-09 14:55:31 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/asteri-lp/', // 👈 this is the critical line for GitHub Pages
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
